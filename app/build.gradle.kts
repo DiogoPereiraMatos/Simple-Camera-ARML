84baf8c27a0dd0bdfd6f4933a7bc16eb84ffef98
DiogoPereiraMatos <pg50340@alunos.uminho.pt>
2023-12-27 15:18:19 +0000

    Add Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,4 +94,5 @@ dependencies {
     implementation(libs.androidx.exifinterface)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.window)
+    implementation("com.google.ar:core:1.40.0")
 }
